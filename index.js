3a18a4f6f867ecd175463d5b4a8d09755a51fe16
Lasana Murray <lmurray@quenk.com>
2016-11-14 15:10:50 -0400

    fix: don't ignore zeros.

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
 function boundary_to_dot(value) {
-	return value.split('][').join('.').split('[').join('.');
+    return value.split('][').join('.').split('[').join('.');
 }
 
 function strip_braces(value) {
-	return value.split('[').join('.').split(']').join('');
+    return value.split('[').join('.').split(']').join('');
 }
 
 function escape_dots(value) {
-	value = value.split('\'');
-	return (value.length < 3) ? value.join('\'') : value.map(function(seg) {
-		if (seg.length < 3) return seg;
-		if ((seg[0] === '.') || (seg[seg.length - 1] === '.')) return seg;
-		return seg.split('.').join('&&');
-	}).join('');
+    value = value.split('\'');
+    return (value.length < 3) ? value.join('\'') : value.map(function(seg) {
+        if (seg.length < 3) return seg;
+        if ((seg[0] === '.') || (seg[seg.length - 1] === '.')) return seg;
+        return seg.split('.').join('&&');
+    }).join('');
 }
 
 function unescape_dots(value) {
-	return value.split('&&').join('.');
+    return value.split('&&').join('.');
 }
 
 function partify(value) {
-	if (!value) return '';
-	return escape_dots(strip_braces(boundary_to_dot('' + value))).split('.');
+    if (!value) return;
+    return escape_dots(strip_braces(boundary_to_dot('' + value))).split('.');
 }
 
 var get = function(o, path) {
 
-	var parts = partify(path);
-	if (parts.length === 1) return o[unescape_dots(parts[0])];
-	if (parts.length === 0) return;
+    var parts = partify(path);
+    if (parts.length === 1) return o[unescape_dots(parts[0])];
+    if (parts.length === 0) return;
 
-	var first = o[parts.shift()];
+    var first = o[parts.shift()];
 
-	return parts.reduce(function(target, prop) {
-		if (!target) return target;
-		return target[unescape_dots(prop)];
-	}, first);
+    return parts.reduce(function(target, prop) {
+        if (target == null) return target;
+        return target[unescape_dots(prop)];
+    }, first);
 };
 
 get.set = function(obj, path, value) {
-	var parts = partify(path);
-	parts.reduce(function(target, prop, i) {
-		prop = unescape_dots(prop);
-		if (parts.length - 1 === i) {
-			target[prop] = value;
-		} else {
-			target[prop] = target[prop] || {};
-		}
-		return target[prop];
+    var parts = partify(path);
+    parts.reduce(function(target, prop, i) {
+        prop = unescape_dots(prop);
+        if (parts.length - 1 === i) {
+            target[prop] = value;
+        } else {
+            target[prop] = target[prop] || {};
+        }
+        return target[prop];
 
 
-	}, obj);
+    }, obj);
 
-	return obj;
+    return obj;
 };
 
 module.exports = get;
--- test.js
+++ test.js
@@ -3,50 +3,73 @@ var must = require('must');
 
 describe('property-seek', function() {
 
-	it('should return get the correct value', function() {
-		var user = {
-			name: {
-				first: 'Joe',
-				last: 'M',
-				'dot.name': 'Joe.M',
-				status: {
-					banned: true
-				}
-			},
-			'dot.value': '...'
-		};
-		must(Properties.get(user, 'name')).be.an.object();
-		must(Properties.get(user, 'name.first')).equal('Joe');
-		must(Properties.get(user, 'name.last')).equal('M');
-		must(Properties.get(user, 'name.status.banned')).equal(true);
-		must(Properties.get(user, 'name[status][banned]')).equal(true);
-				must(Properties.get(user, '\'dot.value\'')).equal('...');
-		must(Properties.get(user, 'name[\'dot.name\']')).equal('Joe.M');
-		must(Properties.get(user, 'nam')).be.undefined();
-	});
+    it('should return get the correct value', function() {
+        var user = {
+            name: {
+                first: 'Joe',
+                last: 'M',
+                'dot.name': 'Joe.M',
+                status: {
+                    banned: true
+                }
+            },
+            'dot.value': '...'
+        };
+        must(Properties.get(user, 'name')).be.an.object();
+        must(Properties.get(user, 'name.first')).equal('Joe');
+        must(Properties.get(user, 'name.last')).equal('M');
+        must(Properties.get(user, 'name.status.banned')).equal(true);
+        must(Properties.get(user, 'name[status][banned]')).equal(true);
+        must(Properties.get(user, '\'dot.value\'')).equal('...');
+        must(Properties.get(user, 'name[\'dot.name\']')).equal('Joe.M');
+        must(Properties.get(user, 'nam')).be.undefined();
+    });
 
-	it('should set the correct value at the correct path', function() {
-		var user = {
-			name: {
-				first: 'Joe',
-				last: 'M',
-				status: {
-					banned: true
-				}
-			}
-		};
-		Properties.set(user, 'name.first', 'Bob');
-		must(user.name.first).equal('Bob');
-		Properties.set(user, 'name.status.banned', false);
-		must(user.name.status.banned).equal(false);
-		Properties.set(user, 'name.middle', 'H');
-		must(user.name.middle).equal('H');
-		Properties.set(user, 'name', 'Bob');
-		must(user.name).equal('Bob');
-		Properties.set(user, 'location.address', 'Mt. Hope');
-		must(user.location.address).equal('Mt. Hope');
-		Properties.set(user, 'location[\'address\']', 'Arima');
-		must(user.location.address).equal('Arima');
+    it('should set the correct value at the correct path', function() {
+        var user = {
+            name: {
+                first: 'Joe',
+                last: 'M',
+                status: {
+                    banned: true
+                }
+            }
+        };
+        Properties.set(user, 'name.first', 'Bob');
+        must(user.name.first).equal('Bob');
+        Properties.set(user, 'name.status.banned', false);
+        must(user.name.status.banned).equal(false);
+        Properties.set(user, 'name.middle', 'H');
+        must(user.name.middle).equal('H');
+        Properties.set(user, 'name', 'Bob');
+        must(user.name).equal('Bob');
+        Properties.set(user, 'location.address', 'Mt. Hope');
+        must(user.location.address).equal('Mt. Hope');
+        Properties.set(user, 'location[\'address\']', 'Arima');
+        must(user.location.address).equal('Arima');
 
-	});
+    });
+
+    it('should not mistreat zeros', function() {
+
+        must(Properties.get({
+            the: {
+                zero: {
+                    value: 0
+                }
+            }
+        }, 'the.zero.value')).be(0)
+
+     must(Properties.get({
+            the: {
+                zero: {
+                    value: '0'
+                }
+            }
+        }, 'the.zero.value')).be('0')
+
+
+
+    });
+
 });
